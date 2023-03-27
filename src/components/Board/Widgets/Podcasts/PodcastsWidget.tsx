374d800d5be7cac77432f46a048e5684d02ea189
onur-celik <onurcelik@me.com>
2023-03-27 20:34:35 +0300

    fix: podcasts widget fix

--- src/components/Board/Widgets/Podcasts/PodcastsWidget.tsx
+++ src/components/Board/Widgets/Podcasts/PodcastsWidget.tsx
@@ -11,7 +11,7 @@ const PodcastsWidget = memo(function ({ id }: { id: string }) {
             <div className="content">
                 <iframe
                     style={{ borderRadius: 12, border: 0 }}
-                    src="https://open.spotify.com/embed/show/2Yvo8QxZf7WlSEsIwKjtX4?utm_source=generator&theme=0"
+                    src="https://open.spotify.com/embed/show/2Yvo8QxZf7WlSEsIwKjtX4?utm_source=generator&theme=1"
                     width="100%"
                     height="152"
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
@@ -20,7 +20,7 @@ const PodcastsWidget = memo(function ({ id }: { id: string }) {
                 <br />
                 <iframe
                     style={{ borderRadius: 12, border: 0 }}
-                    src="https://open.spotify.com/embed/show/4M5Gb71lskQ0Rg6e08uQhi?utm_source=generator&theme=0"
+                    src="https://open.spotify.com/embed/show/4M5Gb71lskQ0Rg6e08uQhi?utm_source=generator&theme=1"
                     width="100%"
                     height="152"
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
@@ -29,7 +29,7 @@ const PodcastsWidget = memo(function ({ id }: { id: string }) {
                 <br />
                 <iframe
                     style={{ borderRadius: 12, border: 0 }}
-                    src="https://open.spotify.com/embed/show/7LYeomavBzwgQhJos7qrms?utm_source=generator&theme=0"
+                    src="https://open.spotify.com/embed/show/7LYeomavBzwgQhJos7qrms?utm_source=generator&theme=1"
                     width="100%"
                     height="152"
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
